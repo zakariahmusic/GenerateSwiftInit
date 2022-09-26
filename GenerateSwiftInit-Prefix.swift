5fa3634c56f89ddda82bf8c8b52f436d42ad480b
Zakariah Music <zakvmusic@gmail.com>
2022-09-26 02:11:37 -0400

    z
    z

---
+++ GenerateSwiftInit-Prefix.swift
@@ -0,0 +1,2 @@
+// Created by Swiftify to force generation of the Swift Bridging Header file (`GenerateSwiftInit-Swift.h`).
+// This file can be safely deleted once your project contains at least one Swift file.
